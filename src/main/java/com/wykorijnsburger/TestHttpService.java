2098ca0eb3d4c0b27dca1689a41788e160d18ed0
Wyko Rijnsburger <wrijnsburger@bol.com>
2017-04-13 09:37:50 +0200

    Fix connection not closing in between tests.

--- src/main/java/com/wykorijnsburger/TestHttpService.java
+++ src/main/java/com/wykorijnsburger/TestHttpService.java
@@ -2,16 +2,17 @@ package com.wykorijnsburger;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
 import reactor.ipc.netty.http.client.HttpClient;
-
-import java.time.Duration;
+import reactor.ipc.netty.resources.PoolResources;
 
 public class TestHttpService {
 
     public static HttpResponseStatus testHttpCallWithUrlEncoding(final String url) {
-        final HttpClient httpClient = HttpClient.create("jsonplaceholder.typicode.com");
+        final HttpClient httpClient = HttpClient.create(ops ->
+            ops.connect("jsonplaceholder.typicode.com", 80)
+                .poolResources(PoolResources.elastic("TYPICODE")));
 
         return httpClient.get(url)
-            .block(Duration.ofMillis(100))
+            .block()
             .status();
     }
 }
