bfbf40140781d241df8d049b18749ee37eab0d66
Wyko Rijnsburger <wrijnsburger@bol.com>
2017-04-13 13:56:25 +0200

    Fix tests.

--- src/main/java/com/wykorijnsburger/TestHttpService.java
+++ src/main/java/com/wykorijnsburger/TestHttpService.java
@@ -1,7 +1,10 @@
 package com.wykorijnsburger;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
+import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.client.HttpClient;
+import reactor.ipc.netty.http.client.HttpClientException;
+import reactor.ipc.netty.http.client.HttpClientResponse;
 import reactor.ipc.netty.resources.PoolResources;
 
 public class TestHttpService {
@@ -12,7 +15,13 @@ public class TestHttpService {
                 .poolResources(PoolResources.elastic("TYPICODE")));
 
         return httpClient.get(url)
-            .block()
-            .status();
+            .map(HttpClientResponse::status)
+            .otherwise(HttpClientException.class, e -> {
+                if (e.status().equals(HttpResponseStatus.NOT_FOUND)) {
+                    return Mono.just(e.status());
+                }
+                return Mono.error(e);
+            })
+            .block();
     }
 }
